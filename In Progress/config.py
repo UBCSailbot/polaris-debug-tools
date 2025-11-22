ac1439c664b7d70ee085957f3201ccb831b6bc19
Breanne Chan <bchan101@students.cs.ubc.ca>
2025-11-21 16:32:55 -0800

    splitting up large main file

---
+++ In Progress/config.py
@@ -0,0 +1,30 @@
+
+
+# SSH Credentials
+hostname = "192.168.0.10"
+username = "sailbot"
+password = "sailbot"
+
+can_line = "can0"
+
+value_label_min_width = 100
+value_label_max_height = 40
+
+value_style = """
+            color: black;
+            font-size: 16px;
+            font-weight: bold;
+            font-family: 'Courier New', monospace;
+            padding: 1px;
+            background-color: #f0f0f0;
+            border: 2px solid #cccccc;
+            border-radius: 3px;
+            margin: 2px;
+        """
+bold_text = "font-weight: bold;"
+
+linewidth = 2
+graph_xlabel = "Time (s)" # all graphs read in seconds
+graph_min_width = 275
+graph_min_height = 250
+scroll_window = 60 # in seconds
