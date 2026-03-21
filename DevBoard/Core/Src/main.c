0369b08fee3d31e492b0cec7cd6c41bf969b663a
Aayush Kolharkar <aayushkolharkar@gmail.com>
2026-03-21 13:48:48 -0700

    .

--- DevBoard/Core/Src/main.c
+++ DevBoard/Core/Src/main.c
@@ -536,19 +536,6 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
   * @param  htim : TIM handle
   * @retval None
   */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  /* USER CODE BEGIN Callback 0 */
-
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM17)
-  {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
-
-  /* USER CODE END Callback 1 */
-}
 
 /**
   * @brief  This function is executed in case of error occurrence.
